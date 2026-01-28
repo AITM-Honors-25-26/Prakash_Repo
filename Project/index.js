5c707cc6cae6a8ddb3ecf5e82cea0105c4099ac5
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-01-28 12:02:25 +0545

    creating server

--- Project/index.js
+++ Project/index.js
@@ -1,3 +1,10 @@
 import http from "http";
 
 const httpServer = http.createServer();
+
+httpServer.listen(9005, "192.168.1.67",(err)=>{
+    if(!err){
+        console.log("server is running on port: ",9005)
+        console.log("Press CTRL + C to discontinue the code......")
+    }
+})
