e313968a4c3ada25feb6784b9f36365d6512d422
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-03-01 08:40:22 +0545

    Update app.js

--- test/app.js
+++ test/app.js
@@ -1,16 +1,16 @@
 import bcrypt from 'bcrypt'
 
 const pass = "Prakash_Budha_Magar"
-
-
 const start = performance.now()
+const salt = await bcrypt.genSalt(10)
 
- 
-const encPass = await bcrypt.hash(pass,10)
+const encPass = await bcrypt.hash(pass, salt)
+
 const end = performance.now()
 
 const timeTaken = (end - start).toFixed(2)
 
+console.log(`Salt: ${salt}`)
 console.log(`Password: ${pass}`)
 console.log(`Hash: ${encPass}`)
-console.log(`Time taken to generate salt and hash: ${timeTaken} ms`)
+console.log(`Time taken to generate hash: ${timeTaken} ms`)
