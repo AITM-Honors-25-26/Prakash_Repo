dd6c197ccdfed91e408e80188d72099a425208fc
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-04-06 14:59:25 +0545

    compleated the homepage where the screen change in mobile is made working till now

--- Frontend/src/components/header/header.tsx
+++ Frontend/src/components/header/header.tsx
@@ -5,16 +5,21 @@ import logo from './../../../img/Logo.png'
 
 const Header: React.FC = () => {
   return (
+
     <header className={styles.header}>
-      <div><img src={logo} className={styles.logo} alt="logo" /></div>
-      <nav className={styles.navLinks}>
-        <a href="/">Home</a>
-        <a href="/features">Special</a>
-        <a href="/contact">About Us</a>
-      </nav>
-      <div><img src={profile} className={styles.profile} alt="Profile" /></div>
+      
+        <img src={logo} className={styles.logo} alt="logo" />
+        
+        <nav className={styles.navLinks}>
+          <a href="/">Home</a>
+          <a href="/features">Special</a>
+          <a href="/contact">About Us</a>
+        </nav>
+        
+        <img src={profile} className={styles.profile} alt="Profile" />
+        
     </header>
   );
 };
 
-export default Header;       
+export default Header;
--- Frontend/src/pages/home/home.page.tsx
+++ Frontend/src/pages/home/home.page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from './../../components/layout/layout';
 import { Link } from 'react-router-dom';
 import profile from '../../../img/bakery photo.png'
+import logo from '../../../img/Logo.png'
 import styles from './homepage.module.scss';
 
 const Homepage: React.FC = () => {
@@ -10,18 +11,14 @@ const Homepage: React.FC = () => {
       <section className={styles.topsection}>
         <div className={styles.photoholder}><img src={profile} className={styles.photo} alt="" /></div>
         <div className={styles.welcomecontent}>
-          <h1>My First Homepage</h1>
-        <p>Testing the homepage.</p>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1>Welcome to the Bakery</h1>
+        <p>Where passion blooms and pastries rise.</p>
         <Link to="/LoginPage">
-          <button>Login Now</button>
+          <button className={styles.logbutton}>Login Now</button>
         </Link>
-
-
-
-
-
         <Link to="/RegisterPage">
-          <button>Register Now</button>
+          <button className={styles.registerbutton}>Register Now</button>
         </Link>
 
         </div>
