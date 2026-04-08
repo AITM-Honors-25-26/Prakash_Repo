b76b8d226fa6895703431a9101bbe0722aee12f7
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-04-08 14:45:13 +0545

    working with forget password

---
+++ Frontend/src/pages/forgotPassword/forgotPassword.page.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_ENDPOINTS } from '../../assets/constants/constants';
+import styles from "./forgotPassword.module.scss"
+import logo from "../../../img/Logo.png"
+import { toast, ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch(API_ENDPOINTS.LOGIN, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email}),
+      });
+
+      let result;
+      try {
+        result = await response.json();
+      } catch {
+        result = {};
+      }
+
+      if (response.ok) {
+        console.log("Full Result Data:", result.data);
+        const userStatus = result.data.status;
+        if(userStatus === false){
+          toast.error("Your account is not activated")
+          return;
+        }
+        const token = result.data?.accessToken;
+
+        if (token) {
+          localStorage.setItem('token', token);
+          toast.success("Login Successful!");
+          navigate('/');
+        } else {
+          toast.error("Login failed: No token received");
+        }
+      } else {
+        toast.error(result.message || "Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Connection error:", error);
+      toast.error("Check if backend is running and CORS is enabled.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+    <ToastContainer position="top-right" autoClose={3000} />
+    
+   <section className={styles.whole}>
+  <div className={styles.leftdisplay}>
+    <img src={logo} alt="Company Logo" />
+  </div>
+
+  <div className={styles.rightdsiplay}>
+    <h2>Login</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" disabled={loading}>
+        {loading ? "Logging in..." : "Login"}
+      </button>
+    </form>
+
+    <div className={styles.links}>
+      <p>Don't have an account? <Link to="/RegisterPage">Register</Link></p>
+      <p><Link to="/">Back to Home</Link></p>
+    </div>
+  </div>
+</section>
+</>
+  );
+};
+
+export default LoginPage;
