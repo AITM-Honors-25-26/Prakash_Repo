56f6aa291f63fa91621ac9849beb3b95f101a076
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-04-09 17:11:04 +0545

    working with reactive ui for contacty us page

--- Frontend/src/pages/ContactUS/ContactUs.Page.tsx
+++ Frontend/src/pages/ContactUS/ContactUs.Page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './../../components/layout/layout';
+import location from '../../../img/logos/location.png'
 
 
 import styles from './ContactUsPage.module.scss';
@@ -11,7 +12,18 @@ const ContactUs: React.FC = () => {
         <div className={styles.header}>
             <h1>Get in touch with us <span>for more information</span></h1>  
         </div>
-        <div></div>
+        <div className={styles.bottomSection}>
+            <div className={styles.bottomSection1}>
+                <img src={location} alt="" />
+                <p>hello</p>
+            </div>
+            <div className={styles.bottomSection2}>
+                <p>this is 2</p>
+            </div>
+            <div className={styles.bottomSection3}>
+                <p>tihs is 3</p>
+            </div>
+        </div>
           
           
 
