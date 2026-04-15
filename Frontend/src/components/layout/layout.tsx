afd87a51d39b6feb12b76b031b2c52b50f4a3ebd
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-04-15 11:30:39 +0545

    testing with the onld the newprogilebar not sucess

--- Frontend/src/components/layout/layout.tsx
+++ Frontend/src/components/layout/layout.tsx
@@ -21,39 +21,3 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 };
 export default Layout;
 
-
-// import React from 'react';
-// import Header from './../header/header';
-// import ProfileBar from '../ProfileBar/profilebar'; 
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import Footer from '../Footer/footer';
-// import styles from './layout.module.scss'; 
-
-// interface LayoutProps {
-//   children: React.ReactNode;
-// }
-
-// const Layout: React.FC<LayoutProps> = ({ children }) => {
-//   return (
-//     <div className={styles.layoutWrapper}>
-//       <Header />
-//       <div className={styles.layoutBody}>
-//         <aside className={styles.sidebarSection}>
-//           <ProfileBar />
-//         </aside>
-
-//         <main className={styles.mainContent}>
-//           <div className={styles.container}>
-//             {children}
-//           </div>
-//         </main>
-//       </div>
-
-//       <ToastContainer position="top-right" autoClose={3000} />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Layout;
