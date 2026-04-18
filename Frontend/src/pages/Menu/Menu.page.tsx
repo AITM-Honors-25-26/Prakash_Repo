0b9de7a4702fe90e0f5d3a5db703747a5747a74e
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-04-18 19:43:38 +0545

    still in the menue

--- Frontend/src/pages/Menu/Menu.page.tsx
+++ Frontend/src/pages/Menu/Menu.page.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './MenuPage.module.scss';
 import Layout from '../../components/layout/layout';
 import bread from '../../../img/bread.png';
+import cartwhite from '../../../img/icons/cart.white.png';
 
 const MenuPage: React.FC = () => {
   return (
     <Layout>
       <div className={styles.main}>
+        
         <div className={styles.topic}>
           <h1>Hot sales</h1>
         </div>
@@ -14,14 +16,21 @@ const MenuPage: React.FC = () => {
         <div className={styles.itemSection}>
             <div className={styles.photosection}>
               <img src={bread} alt="Fresh Bread" />
+              
               <div className={styles.overlay}>
                 <h3>Artisan Bread</h3>
+                <p>This is for the description part</p>
                 <p>$5.99</p>
-                <button>Add to cart</button>
+                
+                <div className={styles.buttonDiv}>
+                  Add to cart 
+                  <img src={cartwhite} alt="Cart icon" />
+                </div>
               </div>
+
+            </div>
           </div>
-          {/* End of itembox */}
-        </div>
+          
       </div>
     </Layout>
   );
