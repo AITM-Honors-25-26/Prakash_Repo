94f3021e21cc269fea83872e7788560b4804b045
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-05-10 19:51:15 +0545

    test

--- Backend/src/modules/table/table.router.js
+++ Backend/src/modules/table/table.router.js
@@ -7,25 +7,8 @@ import { UserRole } from "../../config/constants.js";
 
 const tableRouter = Router();
 
-// Add a new table
-tableRouter.post(
-    '/table/add', 
-    allowUser([UserRole.ADMIN]), 
-    bodyValidator(tableCreateSchema), 
-    tableCtrl.createTable
-);
-
-// Get a list of all tables
-tableRouter.get(
-    '/table/list', 
-    tableCtrl.getAllTables
-);
-
-// Delete a specific table by ID
-tableRouter.delete(
-    '/table/:id', 
-    allowUser([UserRole.ADMIN]), 
-    tableCtrl.deleteTable
-);
+tableRouter.post('/table/add',allowUser([UserRole.ADMIN]),bodyValidator(tableCreateSchema),tableCtrl.createTable);
+tableRouter.get('/table/list',tableCtrl.getAllTables);
+tableRouter.delete('/table/:id',allowUser([UserRole.ADMIN]),tableCtrl.deleteTable);
 
 export default tableRouter;
