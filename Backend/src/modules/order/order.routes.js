2e53c17b8163cbc47316a22a14fb309bfb4a7c93
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-05-21 15:12:26 +0545

    working on routes

--- Backend/src/modules/order/order.routes.js
+++ Backend/src/modules/order/order.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { createOrder, getKitchenOrders, updateOrderStatus } from './order.controller.js';
 import { validateCreateOrder, validateUpdateStatus } from './order.validator.js';
+import allowUser from '../../middleware/auth.middelware.js'; 
+import { UserRole } from '../../config/constants.js';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/order/', validateCreateOrder, createOrder);
-
-orderRouter.get('/order/kitchen', getKitchenOrders);
-orderRouter.patch('/order/:id/status', validateUpdateStatus, updateOrderStatus);
+orderRouter.post('/order/',validateCreateOrder,createOrder);
+orderRouter.get('/order/kitchen',allowUser([UserRole.ADMIN, UserRole.CHEF, UserRole.WAITER]),getKitchenOrders);
+orderRouter.patch('/order/:id/status',allowUser([UserRole.ADMIN, UserRole.CHEF, UserRole.WAITER]),validateUpdateStatus,updateOrderStatus
+);
 
 export default orderRouter;
