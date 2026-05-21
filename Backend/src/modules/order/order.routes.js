6dc7c9463a44a9daf1ff08a720ce84406773fd34
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-05-21 13:22:21 +0545

    working on the order route

---
+++ Backend/src/modules/order/order.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createOrder, getKitchenOrders, updateOrderStatus } from './order.controller.js';
+import { validateCreateOrder, validateUpdateStatus } from './order.validator.js';
+
+const router = express.Router();
+
+router.post('/', validateCreateOrder, createOrder);
+
+router.get('/kitchen', getKitchenOrders);
+router.patch('/:id/status', validateUpdateStatus, updateOrderStatus);
+
+export default router;
