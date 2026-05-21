9fb4342612711ae41f9c0d92c8ef43cff122377e
Prakasg Budha Magar <Prakashbudha2003@gmail.com>
2026-05-21 13:26:46 +0545

    adding route

--- Backend/src/config/router.config.js
+++ Backend/src/config/router.config.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.router.js";
 import menuRouter from "../modules/menu/menu.router.js"; 
 import tableRouter from "../modules/table/table.router.js";
+import orderRouter from "../modules/order/order.routes.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(menuRouter);
-router.use(tableRouter)
+router.use(tableRouter);
+router.use(orderRouter)
 
 export default router;
--- Backend/src/modules/order/order.routes.js
+++ Backend/src/modules/order/order.routes.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { createOrder, getKitchenOrders, updateOrderStatus } from './order.controller.js';
 import { validateCreateOrder, validateUpdateStatus } from './order.validator.js';
 
-const router = express.Router();
+const orderRouter = express.Router();
 
 router.post('/', validateCreateOrder, createOrder);
 
 router.get('/kitchen', getKitchenOrders);
 router.patch('/:id/status', validateUpdateStatus, updateOrderStatus);
 
-export default router;
+export default orderRouter;
